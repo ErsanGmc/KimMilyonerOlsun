691a452aaa7387db0ef5e316f7ffc169a5393dd0
Kaan Dalkıran <kaan51dlkrn@gmail.com>
2024-06-25 17:34:27 +0300

    Initial Commit

---
+++ KimMilyonerOlsun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KimMilyonerOlsun
+//
+//  Created by Trakya11 on 25.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
